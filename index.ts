d046db05061d62872079dcda35f4b33fa6a03461
Damilola Olowookere <damms005@gmail.com>
2021-05-16 02:57:35 +0100

    Switch to TypeScript

--- index.ts
+++ index.ts
@@ -12,6 +12,11 @@ Reply to a post
 */
 
 /*
+-tailwind css
+-hot reload
+*/
+
+/*
 1. Efficacy of your submission: Fundamentally how well your solution is able to achieve the assignment objective and solve the stated problem.
 2. System Design: How well your application will perform at scale
 3. Code quality
@@ -25,5 +30,6 @@ Reply to a post
 /*
 Next stage:
 -implement token refresh
+-registration should ensure email unique
 -reduce collision in image uploads by including user ID in filename
 */
