9167a9c715f7c27f2e836524a92be940428f4990
Damilola Olowookere <damms005@gmail.com>
2021-05-16 02:48:57 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+Registration & login with email & password
+Authentication with JWT
+Send an email to a user after registration
+Password reset
+Publish a post
+Fetch a post
+Delete a post
+Edit a post
+Like & undo post like ??
+Reply to a post
+*/
+
+/*
+1. Efficacy of your submission: Fundamentally how well your solution is able to achieve the assignment objective and solve the stated problem.
+2. System Design: How well your application will perform at scale
+3. Code quality
+	a. Code modularity
+	b. Code documentation - balancing between self-documenting code and comments
+	c. Unit and integration testing
+	d. Adherence to REST standards
+3. Documentation: Readme and Postman/Swagger
+*/
+
+/*
+Next stage:
+-implement token refresh
+-reduce collision in image uploads by including user ID in filename
+*/
